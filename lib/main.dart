db0c054608ad06d6588755696c9027a688807545
Axel Bock <mr.axel.bock@gmail.com>
2020-01-03 19:50:23 +0100

    first 'appy' looking thing

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,10 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text("hi there!"));
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(title: Text('me title!')),
+      body: Text("me BODY!!"),
+    ));
   }
 }
