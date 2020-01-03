55fd4774fd1bcd1f969bf0e7802d163c4ac37c41
Axel Bock <mr.axel.bock@gmail.com>
2020-01-03 20:21:46 +0100

    introducing stateful widgets

--- lib/main.dart
+++ lib/main.dart
@@ -4,19 +4,40 @@ void main() {
   runApp(MyApp());
 }
 
-void buttonPressed() {}
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
 
-class MyApp extends StatelessWidget {
+class MyAppState extends State<MyApp> {
+  var texts = ["A Riddle!", "A Fiddle!", "A Sharmagiddle!"];
+  var cycle = 0;
+
+  void buttonPressed() {
+    // if we do this:
+    //cycle = (cycle + 1) % texts.length;
+    // ... the text will NOT change!
+
+    setState(() {
+      // because we place this IN HERE this works.
+      cycle = (cycle + 1) % texts.length;
+    });
+
+    print("cycle: $cycle");
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(title: Text('me title!')),
             body: Column(children: [
-              Text("A question"),
+              Text(texts[cycle]),
               RaisedButton(
                 child: Text("PRESS! PRESS!"),
-                onPressed: () => print("Hi"),
+                onPressed: buttonPressed,
               )
             ])));
   }
