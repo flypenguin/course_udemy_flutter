f9fbcc7565aed91ddebbf7baa318a08debfe3a32
Axel Bock <mr.axel.bock@gmail.com>
2020-01-12 19:35:19 +0100

    move to transactions and add (ugly) styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,6 +17,29 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: "t01",
+        title: "pizza from eataliano",
+        amount: 8.00,
+        date: DateTime.now()),
+    Transaction(
+        id: "t02",
+        title: "monokai pro license",
+        amount: 9.99,
+        date: DateTime.now()),
+    Transaction(
+        id: "t03",
+        title: "coffee from ernst",
+        amount: 12.99,
+        date: DateTime.now()),
+    Transaction(
+        id: "t04",
+        title: "cake from ernst",
+        amount: 9.40,
+        date: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
@@ -23,32 +47,49 @@ class MyHomePage extends StatelessWidget {
         middle: Text('Flutter App'),
       ),
       child: SafeArea(
-        // column parameters:
-        //     main axis: top-bottom (for row it's left-right)
-        //     cross axis: left-right (for row it's top-bottom)
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            // one possibility: wrap the Text widget
-            Card(
-              child: Container(
-                child: Text("card 1"),
-                width: double.infinity,
-                height: 100,
-                color: Colors.blue,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: transactions.map((t) {
+            return Card(
+              color: Colors.lime,
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    margin: EdgeInsets.all(15),
+                    padding: EdgeInsets.all(10),
+                    height: 60,
+                    width: 80,
+                    alignment: Alignment.centerRight,
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      ),
+                    ),
+                    child: Text(
+                      t.amount.toStringAsFixed(2),
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      ),
+                    ),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        t.title,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(t.date.toString()),
+                    ],
+                  ),
+                ],
               ),
-            ),
-            // 2nd possibility: wrap card.
-            // you see the difference in coloring and Text placements
-            Container(
-              child: Card(child: Text("card 2")),
-              width: double.infinity,
-              height: 100,
-              color: Colors.red,
-            ),
-            Card(child: Text("card 3")),
-          ],
+            );
+          }).toList(),
         ),
       ),
     );
